dc1b967e0d58b526378c69788989db2158d3a2d2
Magfur <33932322+mjafory@users.noreply.github.com>
2023-10-09 12:21:18 -0400

    problem 4

--- problem4.py
+++ problem4.py
@@ -54,3 +54,100 @@
 # Sum across the secondary diagonal: 4 + 5 + 10 = 19
 # Difference: |4 - 19| = 15
 
+
+# I tried no-sense code first to get deep idea for solving the problem. Please ignore it
+
+# a_matrix = [
+#  [11, 2, 4],
+#     [4, 5, 6],
+#     [10, 8, -12]
+# ]
+
+# add_1 = 0
+# add_2 = 0
+# add_1_reverse = 0
+
+
+# for i in range(len(a_matrix)):
+#     add_1 += a_matrix[i][i]
+#     add_1_reverse = a_matrix[::-1]
+#     add_2 += add_1_reverse[i][i]
+#     dif = abs(add_1 - add_2)
+
+# print(a_matrix)
+# print(add_1_reverse)
+# print(add_1)
+# print(add_2)
+# print(dif)
+
+# def dia_deff(a_matrix):
+  
+#   add_1 = 0
+#   add_2 = 0
+#   add_1_reverse = 0
+  
+  
+#   for i in range(len(a_matrix)):
+#       add_1 += a_matrix[i][i]
+#       add_1_reverse = a_matrix[::-1]
+#       add_2 += add_1_reverse[i][i]
+#       dif = abs(add_1 - add_2)
+#       return dif
+
+# a_matrix = [
+#  [11, 2, 4],
+#     [4, 5, 6],
+#     [10, 8, -12]
+# ]
+
+
+# result = dia_deff(a_matrix)
+
+# print(result)
+
+
+# this is solution: 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+        add1 = 0
+        add2 = 0
+        add1_reverse = 0
+        
+        for i in range(len(arr)):
+            add1 += arr[i][i]
+            add2 += arr[i][len(arr) - 1 - i]
+            
+        diff = abs(add1 - add2)
+        return diff
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
